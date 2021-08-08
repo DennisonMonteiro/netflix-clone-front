c9b7d6b2a4070caf42cf07cdafb721adf03680a9
Dennison Monteiro <dennison.cp2@gmail.com>
2021-08-07 22:17:40 -0300

    listing all data from tmdb API in the screen

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import MovieRow from './components/MovieRow'
+import tmdb from './services/tmdb'
+
+import './App.css'
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([])
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await tmdb.getHomeList()
+      setMovieList(list)
+    }
+    loadAll()
+  }, [])
+
+  return(
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  )
+}
