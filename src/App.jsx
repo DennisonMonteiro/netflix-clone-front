85437aac6cecb1b9490481ad70a5d3e5ed59bd10
Dennison Monteiro <dennison.cp2@gmail.com>
2021-08-08 01:27:15 -0300

    Finishing basic Netflix home page. Making responsive.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,70 @@
 import React, { useEffect, useState } from 'react'
 import MovieRow from './components/MovieRow'
 import tmdb from './services/tmdb'
+import FeaturedMovie from './components/FeaturedMovie'
+import Header from './components/Header'
 
 import './App.css'
 
 export default () => {
 
   const [movieList, setMovieList] = useState([])
+  const [featuredData, setFeaturedData] = useState(null)
+  const [blackHeader, setBlackHeader] = useState(false)
 
   useEffect(() => {
     const loadAll = async () => {
       let list = await tmdb.getHomeList()
       setMovieList(list)
+
+      let originals = list.filter(x => x.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv')
+      setFeaturedData(chosenInfo)
     }
     loadAll()
   }, [])
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+    window.addEventListener('scroll', scrollListener)
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
   return(
     <div className="page">
+      <Header black={blackHeader}/>
+
+      { featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+      
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+
+      <footer>
+        Direitos de imagem para <a href="https://www.netflix.com/br/" target="_blank">Netflix</a>  <br />
+        Dados pegos do site <a href="https://www.themoviedb.org/" target="_blank" >the moviedb</a> 
+      </footer>
+      
+      { movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+        </div>
+      }
     </div>
   )
 }
